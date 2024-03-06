9597665c987a911ab3db25210d5ce53988ff89fc
Isaiahmill14 <isaiahxmiller@gmail.com>
2024-03-06 15:05:02 -0500

    Initial commit

---
+++ models/skill.js
@@ -0,0 +1,14 @@
+const skills = [
+    {id: 125223, skill: 'HTML', done: true},
+    {id: 127904, skill: 'CSS', done: true},
+    {id: 139608, skill: 'Java Script', done: true}
+  ];
+  
+  module.exports = {
+    getAll
+  };
+  
+  function getAll() {
+    return skills;
+  }
+ 
