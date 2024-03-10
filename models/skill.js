13b4c258f35b4ad18a367a1dbc91e64afded0110
Isaiahmill14 <isaiahxmiller@gmail.com>
2024-03-10 15:35:07 -0400

    Most fucntionality done, need to adjust CSS & create edit/delete options

--- models/skill.js
+++ models/skill.js
@@ -6,10 +6,31 @@ const skills = [
   ];
   
   module.exports = {
-    getAll
+    getAll,
+    getOne,
+    create,
+    deleteOne,
   };
   
-  function getAll() {
+function deleteOne(id) {
+  id = parseInt(id)
+  const idx = skills.findIndex(skill => skill.id === id)
+  skills.splice(idx, 1)
+}
+
+function create(skill) {
+  skill.id = Date.now() % 1000000
+  skill.done = false
+  console.log(skill)
+  skills.push(skill)
+}
+
+function getAll() {
     return skills;
-  }
- 
+}
+
+function getOne(id) {
+  id = parseInt(id)
+  const skill = skills.find(skill => skill.id === id)
+  return skill
+}
--- routes/skills.js
+++ routes/skills.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var skillsCtrl = require('../controllers/skills')
+var skills = require('../controllers/skills')
 
 /* GET users listing. */
 
-//starts with /skills
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// starts with /skills
+
+//GET all todo --> 
+router.get('/', skillsCtrl.index)
+
+//GET for the form --> /skills/new
+router.get('/new', skillsCtrl.new)
+
+//GET one skill --> /skills/:id
+router.get('/:skillsId', skillsCtrl.show)
+
+//POST a skill --> /skills
+router.post('/', skillsCtrl.create)
+
+//DELETE a skill --> /skills/:id
+router.delete('/:id', skillsCtrl.delete)
 
 module.exports = router;
